cb082024b850c1f79f51e5f3db78dc06add09853
Neil Dhand <neil_dhand@baylor.edu>
2018-09-22 12:45:57 -0500

    responsive layout and side drawer toggleable.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxilary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <div>Toolbar, sidedrawer, backdrop</div>
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+    
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
 
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+    sideDrawerToggleHandler = () =>{
+        this.setState( (prevState) => {
+            return {showSideDrawer: !prevState.sideDrawer}
+        });
+    }
+    
+    render() {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer 
+                open={this.state.showSideDrawer} 
+                closed={this.sideDrawerClosedHandler} />
 
+                <div>Toolbar, sidedrawer, backdrop</div>
 
-export default layout;
+                <main className={classes.Content}>
+                {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+
+export default Layout;
